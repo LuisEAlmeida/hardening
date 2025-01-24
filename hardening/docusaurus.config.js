9fca0e5390e0e0ef412226d533dfe369a821f9b0
LuisEAlmeida <58267390+LuisEAlmeida@users.noreply.github.com>
2025-01-24 02:39:59 -0500

    fix: Correción de la ruta

--- hardening/docusaurus.config.js
+++ hardening/docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://luisealmeida.github.io/',
+  url: 'https://luisealmeida.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/hardening/',
